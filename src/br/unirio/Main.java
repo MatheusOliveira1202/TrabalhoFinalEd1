66f04be52af01ffdb89a2cc7ef0a0a72a12bb97f
Matheus Oliveira <matolisou@gmail.com>
2019-12-04 23:06:54 -0300

    Time count implementation
    -implementation to count one code run

--- src/br/unirio/Main.java
+++ src/br/unirio/Main.java
@@ -4,15 +4,21 @@ public class Main
 {
 	public static void main(String args[])
 	{
-		OrdenacaoTopologica ord = new OrdenacaoTopologica();
-		
-		String nomeEntrada = "C:\\Users\\T-Gamer\\Documents\\Projetos\\Programming Projects\\Eclipse Projects\\Unirio Projects\\ED1\\TrabalhoFinalEd1\\src\\br\\unirio\\entrada.txt";
-		
-		ord.realizaLeitura(nomeEntrada);
-		
-		if(!ord.executa())
-			System.out.println("O conjunto nao é parcialmente ordenado.");
-		else
-			System.out.println("O conjunto é parcialmente ordenado.");
+		/*for(long i = 0; i < 999999999; i++)
+		{
+			long start = System.currentTimeMillis();*/
+			OrdenacaoTopologica ord = new OrdenacaoTopologica();
+			
+			String nomeEntrada = "C:\\Users\\T-Gamer\\Documents\\Projetos\\Programming Projects\\Eclipse Projects\\Unirio Projects\\ED1\\TrabalhoFinalEd1\\src\\br\\unirio\\entrada.txt";
+			
+			ord.realizaLeitura(nomeEntrada);
+			
+			if(!ord.executa())
+				System.out.println("O conjunto nao é parcialmente ordenado.");
+			else
+				System.out.println("O conjunto é parcialmente ordenado.");
+			/*long delay = System.currentTimeMillis() - start;
+			System.out.println("Demorou " + delay + " milissegundos");
+		}*/
 	}
 }
